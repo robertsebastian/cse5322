92d180787a90f827d7f82090ed1f2e7562c11539
Robert Sebastian <rasebastian@gmail.com>
2014-02-28 20:54:38 -0600

    Initial sample program

---
+++ src/classdiagrameditor/ClassDiagramEditorApp.java
@@ -0,0 +1,44 @@
+/*
+ * ClassDiagramEditorApp.java
+ */
+
+package classdiagrameditor;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ClassDiagramEditorApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ClassDiagramEditorView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ClassDiagramEditorApp
+     */
+    public static ClassDiagramEditorApp getApplication() {
+        return Application.getInstance(ClassDiagramEditorApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ClassDiagramEditorApp.class, args);
+    }
+}
